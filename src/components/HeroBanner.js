79d91622f23880030e82135501f7ed1d2f6f1047
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-05-14 13:32:22 +0800

    finalized build before deployment

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -6,8 +6,11 @@ const HeroBanner = () => {
     return (
         <Box sx={{
             mt: {lg: '212px', xs: '70px'},
-            ml: {sm: '50px'}
-        }} position="relative" p= "20">
+            ml: {sm: '50px'},
+            
+        }} 
+        position="relative" 
+        p= "20">
             <Typography color= "#FF2625" fontWeight="600" fontSize="26px">
                     Fitness Club
             </Typography>
